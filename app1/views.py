57bd545051bcce376396d306febe0e44e491d31e
preethi415 <preethib2002@gmail.com>
2024-03-15 13:56:56 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def jin(request):
+    return render(request,'jin.html')
+
+
+
+
+def nithin(request):
+    return render(request,'nithin.html')
+
+
+
